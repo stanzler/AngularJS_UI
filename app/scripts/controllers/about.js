3ce9ccc1c94f5070bac48469089963c773b6c2ea
David <David@david.com>
2020-01-02 17:36:12 -0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularJsUiApp
+ */
+angular.module('angularJsUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularJsUiApp
+ */
+angular.module('angularJsUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
